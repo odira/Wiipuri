dd7647c2c53eab2c4c40ddf5babd81b480584bf3
Vladimir Ilin <wiipuri_dev@icloud.com>
2026-08-15 23:29:53 +0300

    Some changes

---
+++ Tercas/Task/WiiTask/WiiTask/WiiTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiiTaskApp.swift
+//  WiiTask
+//
+//  Created by Vladimir Ilin on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WiiTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
