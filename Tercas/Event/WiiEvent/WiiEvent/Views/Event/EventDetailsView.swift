62e9a625c865b9325dda854e371735ff05cf78b6
Vladimir Ilin <wiipuri_dev@icloud.com>
2026-09-09 00:29:08 +0300

    Some changes

--- Tercas/Event/WiiEvent/WiiEvent/Views/Event/EventDetailsView.swift
+++ Tercas/Event/WiiEvent/WiiEvent/Views/Event/EventDetailsView.swift
@@ -20,7 +20,7 @@ struct EventDetailsView: View {
                             VStack(alignment: .center, spacing: 10) {
                                 CircleImage(image: event.image)
                                 
-                                if let deal = event.deal { 
+                                if let deal = event.deal {
                                     HStack {
                                         Text("\(event.dealTypeAbbrText) №")
                                         Text(deal).bold()
@@ -36,7 +36,6 @@ struct EventDetailsView: View {
                             }
                             .frame(maxWidth: .infinity)
                         }
-                        
                         Section("Справочная информация") {
                             NavigationLink(destination: DescriptionView(for: event.description)) {
                                 Text("Описание")
@@ -51,34 +50,28 @@ struct EventDetailsView: View {
                                 Text("Исполнение по мероприятию")
                             }
                         }
-                        
                         Section("Реквизиты договора/контракта") {
                             LabeledContent("Дата заключения", value: "N/A")
                             LabeledContent("Дата окончания", value: event.endDate ?? "")
                         }
-                        
                         Section("Исполнение договора") {
                             LabeledContent("Год реализации", value: event.years ?? "")
                             LabeledContent("Ответственный исполнитель", value: event.dealSenior ?? "")
                         }
-                        
                         Section("Стоимость мероприятия") {
                             LabeledContent("Общая стоимость (руб.)", value: event.limitTotal ?? 0, format: .number)
                             NavigationLink(destination: EventLimitView(event: event)) {
                                 Text("Стоимость по годам")
                             }
                         }
-                        
                         Section("Орган ОВД") {
                             LabeledContent("Орган ОВД", value: event.unit ?? "")
                             LabeledContent("Город", value: event.city ?? "")
                         }
-                        
                         Section("Оборудование") {
                             LabeledContent("Оборудование", value: event.equipment ?? "")
                             LabeledContent("Вид работ (наименование этапа)", value: event.phase ?? "")
                         }
-                        
                         Section("Подрядчик") {
                             LabeledContent("Контрагент", value: event.dealContractor ?? "")
                             LabeledContent("Субподрядчик", value: event.dealSubcontractor ?? "")
