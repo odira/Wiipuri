e30748be021315d4c0c90d1ae07fbda3d3db3f7a
Vladimir Ilin <wiipuri_dev@icloud.com>
2026-09-24 19:59:20 +0300

    Some changes

--- Tercas/Event/WiiEvent/WiiEvent/Views/Event/HistoryHelper/HistoryRow.swift
+++ Tercas/Event/WiiEvent/WiiEvent/Views/Event/HistoryHelper/HistoryRow.swift
@@ -32,29 +32,42 @@ struct HistoryRow: View {
     private var headerSection: some View {
         HStack(alignment: .top) {
             VStack(alignment: .leading, spacing: 5) {
-                Text("Отправлено")
+                Text("ОТПРАВЛЕНО")
                     .bold()
+                
                 HStack {
-                    Text("письмо")
-                        .font(.footnote)
-                        .padding(5)
-                        .overlay {
-                            RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                .fill(.clear)
-                                .stroke(Color.blue, lineWidth: 1)
-                        }
-                    Text(dateFormatter.string(from: Date.now))
+                    if let recvLetterDate = history.recvLetterDate {
+                        Text(dateFormatter.string(from: recvLetterDate))
+                    } else {
+                        Text("Дата")
+                    }
+                    if let sendLetterNum = history.sendLetterNum {
+                        Text(sendLetterNum)
+                            .font(.footnote)
+                            .padding(5)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 5, style: .continuous)
+                                    .fill(.clear)
+                                    .stroke(Color.blue, lineWidth: 1)
+                            }
+                    } else {
+                        Text("UNDEFINED")
+                            .font(.footnote)
+                            .padding(5)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 5, style: .continuous)
+                                    .fill(.clear)
+                                    .stroke(Color.blue, lineWidth: 1)
+                            }
+                    }
                 }
                 HStack {
-                    Text("отправитель")
-                        .font(.footnote)
-                        .padding(5)
-                        .overlay {
-                            RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                .fill(.clear)
-                                .stroke(Color.blue, lineWidth: 1)
-                        }
-                    Text("ОТПРАВИТЕЛЬ")
+                    if let sendManufacturerId = history.sendManufacturerId {
+                        Text("send Manufacturer Id: \(sendManufacturerId)")
+                    }
+                    if let sendUnitId = history.sendUnitId {
+                        Text("send Unit Id: \(sendUnitId)")
+                    }
                 }
             }
             .frame(maxWidth: .infinity)
@@ -65,7 +78,7 @@ struct HistoryRow: View {
                     .stroke(Color.blue, lineWidth: 1)
             }
             
-            VStack(spacing: 10) {
+            VStack(alignment: .leading, spacing: 10) {
                 Text(dateFormatter.string(from: history.date))
                     .foregroundStyle(.blue)
                     .font(.title3)
@@ -92,9 +105,15 @@ struct HistoryRow: View {
 
             
             VStack(alignment: .leading, spacing: 5) {
-                Text("Получено")
+                Text("ПОЛУЧЕНО")
                     .bold()
+                
                 HStack {
+                    if let recvLetterDate = history.recvLetterDate {
+                        Text(dateFormatter.string(from: recvLetterDate))
+                    } else {
+                        Text("Дата")
+                    }
                     if let recvLetterNum = history.recvLetterNum {
                         Text(recvLetterNum)
                             .font(.footnote)
@@ -114,22 +133,14 @@ struct HistoryRow: View {
                                     .stroke(Color.blue, lineWidth: 1)
                             }
                     }
-                    if let recvLetterDate = history.recvLetterDate {
-                        Text(dateFormatter.string(from: recvLetterDate))
-                    } else {
-                        Text("Дата")
-                    }
                 }
                 HStack {
-                    Text("получатель")
-                        .font(.footnote)
-                        .padding(5)
-                        .overlay {
-                            RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                .fill(.clear)
-                                .stroke(Color.blue, lineWidth: 1)
-                        }
-                    Text("ПОЛУЧАТЕЛЬ")
+                    if let recvManufacturerId = history.recvManufacturerId {
+                        Text("send Manufacturer Id: \(recvManufacturerId)")
+                    }
+                    if let recvUnitId = history.recvUnitId {
+                        Text("send Unit Id: \(recvUnitId)")
+                    }
                 }
             }
             .frame(maxWidth: .infinity)
@@ -139,16 +150,41 @@ struct HistoryRow: View {
                     .fill(.clear)
                     .stroke(Color.blue, lineWidth: 1)
             }
-
+        }
+        
+        VStack(alignment: .leading) {
+            if let answerTo = history.answerTo {
+                Text("Answer to: \(answerTo)")
+            }
+            if let ref = history.ref {
+                Text("Reference to: \(ref)")
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .overlay {
+            RoundedRectangle(cornerRadius: 3, style: .continuous)
+                .fill(.clear)
+                .stroke(Color.blue, lineWidth: 1)
         }
     }
     
     
     @ViewBuilder
     private var textBodyText: some View {
-        HStack {
+        VStack {
             Text(history.history)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                .multilineTextAlignment(.leading)
+                .padding(5)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5, style: .continuous)
+                        .fill(.clear)
+                        .stroke(Color.blue, lineWidth: 1)
+                }
+            
+            Text(history.note ?? "")
+                .frame(maxWidth: .infinity, alignment: .leading)
                 .multilineTextAlignment(.leading)
                 .padding(5)
                 .overlay {
